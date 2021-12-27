2eae3e3bc6601910056e5f929805ed50610cb476
szmolkal <szmolkal@gmail.com>
2021-12-27 23:00:40 +0100

    Eljutottunk a MySqlDataReader objektum létrehozásáig. A MySqlDataReader kiolvasása előtt létre kell hozni egy Autok osztalyt, amely tárolni fogja az adatbázis rekordjait. Tehát most létre kell hozni egy Autok osztályt.

--- Form1.cs
+++ Form1.cs
@@ -13,8 +13,11 @@ namespace WF_Autok_Adatbazis
 {
     public partial class Form_Autok_Nyito : Form
     {
-        MySqlConnection connection = null;
-        MySqlCommand sqlCommand = null;
+        //-- MySqlConnection Represents an open connection to a MySQL Server database. This class cannot be inherited.
+        MySqlConnection mysqlConnection = null;
+
+        //-- MySqlCommand Represents a SQL statement to execute against a MySQL database. This class cannot be inherited.
+        MySqlCommand mysqlCommand = null;
 
         public Form_Autok_Nyito()
         {
@@ -23,15 +26,16 @@ namespace WF_Autok_Adatbazis
          
         private void Form_Autok_Nyito_Load(object sender, EventArgs e)
         {
+            //-- MySqlConnectionStringBuilder Provides a base class for strongly typed connection string builders over MySQL connections.
             MySqlConnectionStringBuilder sb = new MySqlConnectionStringBuilder();
             sb.Server = "localhost";
             sb.UserID = "root";
             sb.Password = "";
             sb.Database = "autok";
-            sb.CharacterSet = "utf8";
-            connection = new MySqlConnection(sb.ToString());
-            sqlCommand = connection.CreateCommand();
-            
+            sb.CharacterSet = "utf8"; 
+            mysqlConnection = new MySqlConnection(sb.ToString());
+            //sqlCommand = connection.CreateCommand();
+            mysqlCommand = new MySqlCommand(sb.Server, mysqlConnection);
             AutokAdatainakBetolteseAdatbazisbol();
             
         }
@@ -40,13 +44,13 @@ namespace WF_Autok_Adatbazis
         {
             try
             {
-                connection.Open();
-                sqlCommand.CommandText = "SELECT * FROM `autok`;";
-                using (MySqlDataReader dr = sqlCommand.ExecuteReader())
+                mysqlConnection.Open();
+                mysqlCommand.CommandText = "SELECT * FROM `autok`;";
+                using (MySqlDataReader dr = mysqlCommand.ExecuteReader())
                 {
                     while (dr.Read())
                     {
-
+                        
                     }
                 }
             }
