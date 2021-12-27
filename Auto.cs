32099c5b232d6e21660334c9a2b4e845be61e68c
szmolkal <szmolkal@gmail.com>
2021-12-27 23:10:11 +0100

    Az Auto osztály létrehozása, a tagváltozók és tagfüggvények beállítása.

---
+++ Auto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WF_Autok_Adatbazis
+{
+    class Auto
+    {
+        int id = 0;
+        string rendszam = null;
+        string gyartmany = null;
+        string tius = null;
+
+        public Auto(int id, string rendszam, string gyartmany, string tius)
+        {
+            this.id = id;
+            this.rendszam = rendszam;
+            this.gyartmany = gyartmany;
+            this.tius = tius;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Rendszam { get => rendszam; set => rendszam = value; }
+        public string Gyartmany { get => gyartmany; set => gyartmany = value; }
+        public string Tius { get => tius; set => tius = value; }
+    }
+}
